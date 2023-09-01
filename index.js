8694debf75c7941d4dd62d138f63851df3eec7c4
roque solomita <rooque.gsm@gmail.com>
2023-09-01 16:22:53 -0300

    arrow functions, sugar sintax

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-const characterSets = {
-    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    lowercase: "abcdefghijklmnopqrstuvwxyz",
-    numbers: "0123456789",
-    special: "!@#$%^&*",
-  };
-  
-  document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
     const generateBtn = document.getElementById("generateBtn");
     generateBtn.addEventListener("click", generatePassword);
   
@@ -14,17 +7,8 @@ const characterSets = {
   });
   
   function generatePassword() {
-    const siteNameInput = document.getElementById("siteName");
-    const siteName = siteNameInput.value.trim();
-  
-    if (siteName === "") {
-        alert("Please enter a valid website name.");
-        return;
-    }
-  
-    const lengthInput = document.getElementById("length");
-    const length = parseInt(lengthInput.value);
-  
+    const siteName = document.getElementById("siteName").value.trim();
+    const length = parseInt(document.getElementById("length").value);
     const hasUppercase = document.getElementById("uppercase").checked;
     const hasLowercase = document.getElementById("lowercase").checked;
     const hasNumbers = document.getElementById("numbers").checked;
@@ -33,43 +17,34 @@ const characterSets = {
     let pool = "";
     let password = "";
   
-    if (hasUppercase) {
-        pool += characterSets.uppercase;
-    }
-    if (hasLowercase) {
-        pool += characterSets.lowercase;
-    }
-    if (hasNumbers) {
-        pool += characterSets.numbers;
-    }
-    if (hasSpecial) {
-        pool += characterSets.special;
+    if (hasUppercase) pool += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (hasLowercase) pool += "abcdefghijklmnopqrstuvwxyz";
+    if (hasNumbers) pool += "0123456789";
+    if (hasSpecial) pool += "!@#$%^&*";
+  
+    if (!pool) {
+      const passwordDisplay = document.getElementById("passwordDisplay");
+      passwordDisplay.textContent = "Select at least one character type.";
+      return;
     }
   
-    if (pool === "") {
-        const passwordDisplay = document.getElementById("passwordDisplay");
-        passwordDisplay.textContent = "Select at least one character type.";
-        return;
-    }
-  
-    const poolLength = pool.length;
     for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * poolLength);
-        password += pool[randomIndex];
+      const randomIndex = Math.floor(Math.random() * pool.length);
+      password += pool[randomIndex];
     }
   
     const passwordDisplay = document.getElementById("passwordDisplay");
     passwordDisplay.textContent = password;
   
     const copyBtn = document.getElementById("copyBtn");
-    copyBtn.addEventListener("click", function () {
-        const textArea = document.createElement("textarea");
-        textArea.value = password;
-        document.body.appendChild(textArea);
-        textArea.select();
-        document.execCommand("copy");
-        document.body.removeChild(textArea);
-        alert("Password copied to clipboard!");
+    copyBtn.addEventListener("click", () => {
+      const textArea = document.createElement("textarea");
+      textArea.value = password;
+      document.body.appendChild(textArea);
+      textArea.select();
+      document.execCommand("copy");
+      document.body.removeChild(textArea);
+      alert("Password copied to clipboard!");
     });
   
     savePassword(siteName, password);
@@ -77,23 +52,25 @@ const characterSets = {
   
   function savePassword(site, password) {
     const storedData = JSON.parse(localStorage.getItem("passwords")) || {};
-    if (!(site in storedData)) {
-        storedData[site] = password;
-        localStorage.setItem("passwords", JSON.stringify(storedData));
-        alert(`Password saved for site '${site}'.`);
+    if (!storedData[site]) {
+      storedData[site] = password;
+      localStorage.setItem("passwords", JSON.stringify(storedData));
+      alert(`Password saved for site '${site}'.`);
     } else {
-        alert(`Password already exists for site '${site}'.`);
+      alert(`Password already exists for site '${site}'.`);
     }
   }
   
   function searchPasswords() {
     const searchSiteInput = document.getElementById("searchSiteInput").value.trim();
     const storedData = JSON.parse(localStorage.getItem("passwords")) || {};
-    if (searchSiteInput in storedData) {
-        const sitePassword = storedData[searchSiteInput];
-        const passwordDisplay = document.getElementById("passwordDisplay");
-        passwordDisplay.textContent = sitePassword;
+    const sitePassword = storedData[searchSiteInput];
+    const passwordDisplay = document.getElementById("passwordDisplay");
+  
+    if (sitePassword) {
+      passwordDisplay.textContent = sitePassword;
     } else {
-        alert(`Site '${searchSiteInput}' not found.`);
+      alert(`Site '${searchSiteInput}' not found.`);
     }
   }
+  
